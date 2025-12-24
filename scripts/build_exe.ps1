f9407a46a5f7df5fe7d74562f8869d2bbb3d23d7
Maho Nishizumi <WhoisPanzer@proton.me>
2025-12-24 04:59:28 -0300

    Delete scripts/build_exe.ps1

--- scripts/build_exe.ps1
+++
@@ -1,4 +0,0 @@
-# Requires PyInstaller and PySide6 installed
-python -m pip install jp2subs[gui] pyinstaller
-pyinstaller --name jp2subs-gui --onefile --noconfirm --add-data "README.md;." --console -m jp2subs.gui.main
-
